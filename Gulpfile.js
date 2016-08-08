70c20b8eaf8889c36255dcca7fefa7e76f553e87
netecodes <dotneko@gmail.com>
2016-08-08 15:22:56 +0200

    add animation

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,35 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
-	bourbon = require('node-bourbon').includePaths;
+	bourbon = require('node-bourbon').includePaths,
+	plumber = require("gulp-plumber"),
+    browserSync = require("browser-sync");
 
-	gulp.task('styles', function(){
+
+	// Spin up a server
+	gulp.task("browserSync", function() {
+	  browserSync({
+	    server: {
+	      baseDir: ""
+	    }
+	  })
+	});
+
+	//Sass compile stask
+	gulp.task('sass', function(){
 		gulp.src('sass/**/*.sass')
+		.pipe(plumber())
 		.pipe(sass({
 			includePaths: bourbon
 		}).on('error', sass.logError))
 		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.reload({
+        stream: true
+      }))
 	});
 
-	//Watch task
-	gulp.task('default', function(){
-		gulp.watch('sass/**/*.sass',['styles']);
-	});
+// Live reload anytime a file changes
+	gulp.task("default", ["browserSync", "sass"], function() {
+  		gulp.watch(["sass/**/*.sass"], ["sass"]);
+  		gulp.watch("js/**/*.js").on("change", browserSync.reload);
+  		gulp.watch("dist/*.html").on("change", browserSync.reload);
+});
--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ $(function() {
 	smoothScroll(300);
 	workBelt();
 	workLoad();
+	scrollMagic();
+
 })
 
 function smoothScroll (duration) {
@@ -51,3 +53,134 @@ function workLoad() {
 		
 	})
 }
+
+function scrollMagic() {
+	
+	var navbar = $('#nav'),
+		intro_title = $('#intro-title'),
+		intro_kicker = $('#intro-kicker')
+		tl = new TimelineLite();
+
+	//header intro animation
+
+	tl
+		.to(intro_title, 0.7, {autoAlpha:1, y: 20, ease: Power1.easeOut}, 0.4)
+		.to(intro_kicker, 1, {autoAlpha:1, y: 20})
+		.from(navbar, 1, {x: 200})
+
+	// Init ScrollMagic
+	var controller = new ScrollMagic.Controller();
+
+	//Nav Scene
+    var navScene = new ScrollMagic.Scene({
+        triggerElement: '#about-me',
+        triggerHook: 0.1,
+   		})
+	    .setClassToggle('.nav--container', 'is-dark')
+	 //    .addIndicators({
+		// 	name: 'Nav scene',
+		// 	colorTrigger: 'black',
+		// 	colorStart: 'green'
+		// })
+	    .addTo(controller);
+	
+	//About Scene
+	var $about_title =$('#about-me').find('h3'),
+		$face_lockup = $('.face-lockup'),
+		$about = $('.about'),
+		aboutTl = new TimelineMax();
+
+		aboutTl
+			.from($about_title, 1, {autoAlpha:0, y:-30, ease: Power2.easeOut})
+			.from($face_lockup, 1, {autoAlpha:0, y:-30}, 0.2)
+			.from($about, 1, {autoAlpha:0, y:30})
+
+	var aboutScene = new ScrollMagic.Scene({
+		triggerElement: '#about-me',
+		triggerHook: 0.6,
+	})
+	.setTween(aboutTl)
+	// .addIndicators({
+	// 	name: 'About scene',
+	// 	colorTrigger: 'black',
+	// 	colorStart: 'green'
+	// })
+	.addTo(controller);
+
+
+	//Skills Scene
+	var $skill_bar = $('.skill-bar'),
+		$skill_title =$('#skills').find('h3'),
+		$skill_lockup = $('.skill-lockup'),
+		skillTl = new TimelineMax();
+
+		skillTl
+			.from($skill_title, 0.4, {autoAlpha:0, y:-30, ease: Power2.easeOut})
+			.from($skill_lockup, 1, {autoAlpha:0, y:10})
+			.staggerFrom($skill_bar, 1, {width:0, ease: Power3.easeOut}, '-=1')
+
+
+	var aboutScene = new ScrollMagic.Scene({
+		triggerElement: '#skills',
+		triggerHook: 0.6,
+	})
+	.setTween(skillTl)
+	// .addIndicators({
+	// 	name: 'Skills scene',
+	// 	colorTrigger: 'blue',
+	// 	colorStart: 'green'
+	// })
+	.addTo(controller);
+
+	//Work Scene
+	var $thumb_unit = $('.thumb-unit'),
+		$work_title = $('#work').find('h3'),
+		workTl = new TimelineMax();
+		
+		workTl
+			.staggerFrom($thumb_unit, 0.4, {autoAlpha: 0, rotation: -10, ease: Bounce.easeIn}, 0.2)
+			.from($work_title, 1, {autoAlpha:0, y:-30, ease:Linear.easeOut}, '-=0.2')
+		
+		
+
+	var aboutScene = new ScrollMagic.Scene({
+		triggerElement: '#work',
+		triggerHook: 0.6,
+	})
+	.setTween(workTl)
+	// .addIndicators({
+	// 	name: 'work scene',
+	// 	colorTrigger: 'blue',
+	// 	colorStart: 'green'
+	// })
+	.addTo(controller);
+
+	//Contact Scene
+		var $contact_title =$('#contact').find('h3'),
+			$section_contact = $('.section-contact'),
+			$section_form= $('.section-form'),
+			$submit_btn= $('input[type="submit"]')
+			contactTl = new TimelineMax();
+
+		contactTl
+			.from($contact_title, 0.4, {autoAlpha:0, y:-30, ease:Linear.easeOut})
+			.from($section_contact, 0.4, {autoAlpha:0}, 0.5)
+			.from($section_form, 0.4, {autoAlpha:0, y:30}, '-=0.3')
+			.to($submit_btn, 0.2, {autoAlpha:1}, '-=0.2')
+			.to($submit_btn, 0.05, {scale:1.2, repeat: 1, repeatDelay:0.2, yoyo:true}, '+=0.5')
+
+
+	var aboutScene = new ScrollMagic.Scene({
+		triggerElement: '#contact',
+		triggerHook: 0.5,
+	})
+	.setTween(contactTl)
+	// .addIndicators({
+	// 	name: 'Contact scene',
+	// 	colorTrigger: 'blue',
+	// 	colorStart: 'green'
+	// })
+	.addTo(controller);
+
+}
+
