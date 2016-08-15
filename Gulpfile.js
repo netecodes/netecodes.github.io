8ddb8de11c7fa92157ed02a4acca27fdb85ec6e3
netecodes <dotneko@gmail.com>
2016-08-15 11:25:59 +0200

    change style, refactor sass

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,35 +1,70 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	bourbon = require('node-bourbon').includePaths,
-	plumber = require("gulp-plumber"),
-    browserSync = require("browser-sync");
+const gulp = require('gulp'),
+			sass = require('gulp-sass'),
+			bourbon = require('node-bourbon').includePaths,
+			plumber = require("gulp-plumber"),
+			uglify = require("gulp-uglify"),
+			cleanCSS = require('gulp-clean-css'),
+			sourcemaps = require("gulp-sourcemaps"),
+			imagemin = require("gulp-imagemin"),
+			htmlmin = require("gulp-htmlmin"),
+	    bs = require("browser-sync").create();
 
-
-	// Spin up a server
-	gulp.task("browserSync", function() {
-	  browserSync({
-	    server: {
-	      baseDir: ""
-	    }
-	  })
+	//Stars the server
+	bs.init({
+		server: "./dist"
 	});
 
-	//Sass compile stask
-	gulp.task('sass', function(){
-		gulp.src('sass/**/*.sass')
+
+
+
+	//Optimize Images
+	gulp.task('images', () =>
+		gulp.src('src/assets/**/*')
+				.pipe(plumber())
+				.pipe(imagemin())
+				.pipe(gulp.dest('dist/assets'))
+				.pipe(bs.stream())
+	);
+
+	//HTML compile Task
+	gulp.task('html', () =>
+		gulp.src('src/*.html')
+				.pipe(plumber())
+				.pipe(htmlmin({collapseWhitespace: true}))
+				.pipe(gulp.dest('dist'))
+				.pipe(bs.stream())
+	);
+
+	//Sass compile Task
+	gulp.task('sass', () =>
+		gulp.src('src/sass/style.sass')
+				.pipe(plumber())
+				.pipe(sourcemaps.init())
+					.pipe(sass({
+						includePaths: bourbon
+					}).on('error', sass.logError))
+					.pipe(cleanCSS())
+				.pipe(sourcemaps.write())
+				.pipe(gulp.dest('dist/css'))
+				.pipe(bs.stream())
+	);
+
+	//JavaScript compile stask
+	gulp.task('scripts', () =>
+		gulp.src('src/js/app.js')
 		.pipe(plumber())
-		.pipe(sass({
-			includePaths: bourbon
-		}).on('error', sass.logError))
-		.pipe(gulp.dest('./css'))
-		.pipe(browserSync.reload({
-        stream: true
-      }))
-	});
+		.pipe(sourcemaps.init())
+			.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('dist/js'))
+		.pipe(bs.stream({once: true}))
+	);
+
 
 // Live reload anytime a file changes
-	gulp.task("default", ["browserSync", "sass"], function() {
-  		gulp.watch(["sass/**/*.sass"], ["sass"]);
-  		gulp.watch("js/**/*.js").on("change", browserSync.reload);
-  		gulp.watch("dist/*.html").on("change", browserSync.reload);
+	gulp.task("default", ["html","images","sass","scripts"], () => {
+  		gulp.watch(["src/sass/**/*.sass", "src/sass/**/*.scss"], ["sass"]);
+			gulp.watch(["src/js/**/*.js"], ["scripts"]);
+			gulp.watch(["src/assets/**/*"], ["images"]);
+			gulp.watch(["src/*.html"], ["html"]);
 });
